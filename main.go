c4abc1d8a192a9d03eaed4bc08ef86b414081a69
João Pereira <joaodrp@gmail.com>
2019-04-09 19:44:27 +0100

    Almost ready

--- main.go
+++ main.go
@@ -36,13 +36,221 @@ var (
 		6: "INFO",
 		7: "DEBUG",
 	}
+
+	// Additional fields that have a special behaviour
+	specialFields = map[string]string{
+		"app":    "_app",
+		"logger": "_logger",
+	}
 )
 
-const (
-	timeFormat = "2006-01-02 15:04:05.000"
-	appKey     = "_app"
-	loggerKey  = "_logger"
-)
+const timeFormat = "2006-01-02 15:04:05.000"
+
+type fullMessage string
+
+func (fm fullMessage) String() string {
+	if len(fm) == 0 {
+		return ""
+	}
+
+	var sb strings.Builder
+	sb.WriteString("\n\t")
+	sb.WriteString(strings.ReplaceAll(string(fm), "\n", "\n\t"))
+
+	return sb.String()
+}
+
+type unixTimestamp float64
+
+func (t unixTimestamp) String() string {
+	sec, dec := math.Modf(float64(t))
+	tm := time.Unix(int64(sec), int64(dec*(1e9)))
+	str := tm.Format(timeFormat)
+	return fmt.Sprintf("[%s]", str)
+}
+
+type syslogLevel int
+
+func (l syslogLevel) String() string {
+	return levelToName[int(l)]
+}
+
+type additionalField struct {
+	key   string
+	value interface{}
+}
+
+func (af additionalField) String() string {
+	return fmt.Sprintf("%s=%v", strings.TrimPrefix(af.key, "_"), af.value)
+}
+
+func (af additionalField) special() bool {
+	for _, v := range specialFields {
+		if af.key == v {
+			return true
+		}
+	}
+	return false
+}
+
+type additionalFields []additionalField
+
+func (afs additionalFields) String() string {
+	if len(afs) == 0 {
+		return ""
+	}
+	var sb strings.Builder
+	i := 0
+	for _, af := range afs {
+		fmt.Printf("<%v>\n", af.special())
+		if af.special() {
+			continue
+		}
+		if i > 0 {
+			sb.WriteString(" ")
+		}
+		sb.WriteString(af.String())
+		i++
+	}
+	return sb.String()
+}
+
+type gelf struct {
+	version          string
+	host             string
+	shortMessage     string
+	fullMessage      fullMessage
+	timestamp        unixTimestamp
+	level            syslogLevel
+	additionalFields additionalFields
+}
+
+type dict map[string]interface{}
+
+func (d dict) findByKeyAndCastToString(key string, required bool) (string, error) {
+	val, ok := d[key]
+	if !ok && required {
+		return "", fmt.Errorf("%s not found", key)
+	}
+	s, ok := val.(string)
+	if !ok {
+		return "", fmt.Errorf("%s is not a valid string", key)
+	}
+	delete(d, key)
+	return s, nil
+}
+
+func (d dict) findByKeyAndCastToFloat64(key string, required bool) (float64, error) {
+	val, ok := d[key]
+	if !ok && required {
+		return 0, fmt.Errorf("%s not found", key)
+	}
+	n, ok := val.(float64)
+	if !ok {
+		return 0, fmt.Errorf("%s is not a valid number", key)
+	}
+	delete(d, key)
+	return n, nil
+}
+
+func (g *gelf) UnmarshalJSON(data []byte) error {
+	d := dict{}
+	if err := json.Unmarshal(data, &d); err != nil {
+		return err
+	}
+
+	v, err := d.findByKeyAndCastToString("version", true)
+	if err != nil {
+		return err
+	}
+	g.version = v
+
+	h, err := d.findByKeyAndCastToString("host", true)
+	if err != nil {
+		return err
+	}
+	g.host = h
+
+	sm, err := d.findByKeyAndCastToString("short_message", true)
+	if err != nil {
+		return err
+	}
+	g.shortMessage = sm
+
+	fm, err := d.findByKeyAndCastToString("full_message", false)
+	if err != nil {
+		return err
+	}
+	g.fullMessage = fullMessage(fm)
+
+	t, err := d.findByKeyAndCastToFloat64("timestamp", true)
+	if err != nil {
+		return err
+	}
+	g.timestamp = unixTimestamp(t)
+
+	l, err := d.findByKeyAndCastToFloat64("level", true)
+	if err != nil {
+		return err
+	}
+	g.level = syslogLevel(l)
+
+	for k, v := range d {
+		if strings.HasPrefix(k, "_") {
+			af := additionalField{key: k, value: v}
+			g.additionalFields = append(g.additionalFields, af)
+		}
+	}
+	return nil
+}
+
+func (g *gelf) findAdditionalFieldValueByKey(key string) string {
+	for _, af := range g.additionalFields {
+		if af.key == key {
+			return af.value.(string)
+		}
+	}
+	return ""
+}
+
+func (g *gelf) app() string {
+	return g.findAdditionalFieldValueByKey(specialFields["app"])
+}
+
+func (g *gelf) logger() string {
+	return g.findAdditionalFieldValueByKey(specialFields["logger"])
+}
+
+func (g *gelf) pretty() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%s %s: ", g.timestamp, g.level))
+
+	a := g.app()
+	if len(a) > 0 {
+		sb.WriteString(a)
+	}
+
+	l := g.logger()
+	if len(l) > 0 {
+		if len(a) > 0 {
+			sb.WriteString("/")
+		}
+		sb.WriteString(l)
+	}
+
+	if h := g.host; len(h) > 0 {
+		if len(a) > 0 || len(l) > 0 {
+			sb.WriteString(" on ")
+		}
+		sb.WriteString(h)
+	}
+
+	sb.WriteString(fmt.Sprintf(": %s", g.shortMessage))
+	sb.WriteString(fmt.Sprintf(" %s", g.additionalFields))
+	sb.WriteString(g.fullMessage.String())
+
+	return sb.String()
+}
 
 type prettyPrinter struct {
 	reader *bufio.Scanner
@@ -54,6 +262,17 @@ func newPrettyPrinter(r io.Reader, w io.Writer) *prettyPrinter {
 		reader: bufio.NewScanner(r),
 		writer: w,
 	}
+}
+
+func (h *prettyPrinter) processLine(b []byte) error {
+	g := &gelf{}
+	if err := json.Unmarshal(b, g); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintln(h.writer, g.pretty()); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (h *prettyPrinter) run() error {
@@ -70,217 +289,6 @@ func (h *prettyPrinter) run() error {
 		return err
 	}
 	return nil
-}
-
-func msToTime(ms float64) time.Time {
-	sec, dec := math.Modf(ms)
-	return time.Unix(int64(sec), int64(dec*(1e9)))
-}
-
-func (h *prettyPrinter) processLine(b []byte) error {
-	g := &gelf{}
-	if err := json.Unmarshal(b, g); err != nil {
-		return err
-	}
-	if _, err := fmt.Fprintln(h.writer, g.pretty()); err != nil {
-		return err
-	}
-	return nil
-}
-
-type gelf struct {
-	version          string
-	host             string
-	shortMessage     string
-	fullMessage      string
-	timestamp        float64
-	Level            int
-	additionalFields []additionalField
-}
-
-type additionalField struct {
-	key   string
-	value interface{}
-}
-
-func findByKeyAndCastToString(m map[string]interface{}, key string, required bool) (string, error) {
-	val, ok := m[key]
-	if !ok && required {
-		return "", fmt.Errorf("%s not found", key)
-	}
-	s, ok := val.(string)
-	if !ok {
-		return "", fmt.Errorf("%s is not a valid string", key)
-	}
-	return s, nil
-}
-
-func findByKeyAndCastToFLoat64(m map[string]interface{}, key string, required bool) (float64, error) {
-	val, ok := m[key]
-	if !ok && required {
-		return 0, fmt.Errorf("%s not found", key)
-	}
-	n, ok := val.(float64)
-	if !ok {
-		return 0, fmt.Errorf("%s is not a valid number", key)
-	}
-	return n, nil
-}
-
-func (g *gelf) UnmarshalJSON(data []byte) error {
-	m := make(map[string]interface{})
-	if err := json.Unmarshal(data, &m); err != nil {
-		return err
-	}
-
-	v, err := findByKeyAndCastToString(m, "version", true)
-	if err != nil {
-		return err
-	}
-	g.version = v
-	delete(m, "version")
-
-	h, err := findByKeyAndCastToString(m, "host", true)
-	if err != nil {
-		return err
-	}
-	g.host = h
-	delete(m, "host")
-
-	sm, err := findByKeyAndCastToString(m, "short_message", true)
-	if err != nil {
-		return err
-	}
-	g.shortMessage = sm
-	delete(m, "short_message")
-
-	fm, err := findByKeyAndCastToString(m, "full_message", false)
-	if err != nil {
-		return err
-	}
-	g.fullMessage = fm
-	delete(m, "full_message")
-
-	t, err := findByKeyAndCastToFLoat64(m, "timestamp", true)
-	if err != nil {
-		return err
-	}
-	g.timestamp = t
-	delete(m, "timestamp")
-
-	l, err := findByKeyAndCastToFLoat64(m, "level", true)
-	if err != nil {
-		return err
-	}
-	g.Level = int(l)
-	delete(m, "level")
-
-	for k, v := range m {
-		if strings.HasPrefix(k, "_") {
-			af := additionalField{key: strings.TrimPrefix(k, "_"), value: v}
-			g.additionalFields = append(g.additionalFields, af)
-		}
-	}
-	return nil
-}
-
-// Implement the String interface for pretty printing Items
-// func (i Item) String() string {
-// 	return fmt.Sprintf("Item: %s, %d", i.Item1, i.Item2)
-// }
-
-func (g *gelf) prettyTime() string {
-	t := msToTime(g.timestamp).Format(timeFormat)
-	return fmt.Sprintf("[%s]", t)
-}
-
-func (g *gelf) prettyLevel() string {
-	return levelToName[g.Level]
-}
-
-func (g *gelf) findAddFieldValueByKey(key string) interface{} {
-	for _, af := range g.additionalFields {
-		if af.key == key {
-			return af.value
-		}
-	}
-	return ""
-}
-
-func (g *gelf) prettyApp() string {
-	return fmt.Sprintf("%v", g.findAddFieldValueByKey(appKey))
-}
-
-func (g *gelf) prettyLogger() string {
-	return fmt.Sprintf("%v", g.findAddFieldValueByKey(loggerKey))
-}
-
-func (g *gelf) prettyHost() string {
-	return g.host
-}
-
-func (g *gelf) prettyShortMessage() string {
-	return g.shortMessage
-}
-
-func (g *gelf) prettyFullMessage() string {
-	if len(g.fullMessage) == 0 {
-		return ""
-	}
-
-	var sb strings.Builder
-	sb.WriteString("\n\t")
-	sb.WriteString(strings.ReplaceAll(g.fullMessage, "\n", "\n\t"))
-
-	return sb.String()
-}
-
-func (g *gelf) prettyAdditionalFields() string {
-	if len(g.additionalFields) == 0 {
-		return ""
-	}
-	var sb strings.Builder
-	for i, af := range g.additionalFields {
-		if i > 0 {
-			sb.WriteString(" ")
-		}
-		sb.WriteString(fmt.Sprintf("%s=%v", af.key, af.value))
-	}
-	return sb.String()
-}
-
-func (g *gelf) pretty() string {
-	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("%s %s: ", g.prettyTime(), g.prettyLevel()))
-
-	a := g.prettyApp()
-	if len(a) > 0 {
-		sb.WriteString(a)
-	}
-
-	l := g.prettyLogger()
-	if len(l) > 0 {
-		if len(a) > 0 {
-			sb.WriteString("/")
-		}
-		sb.WriteString(l)
-	}
-
-	if h := g.prettyHost(); len(h) > 0 {
-		if len(a) > 0 || len(l) > 0 {
-			sb.WriteString("on ")
-		}
-		sb.WriteString(h)
-	}
-
-	sb.WriteString(fmt.Sprintf(": %s", g.prettyShortMessage()))
-	if af := g.prettyAdditionalFields(); len(af) > 0 {
-		sb.WriteString(fmt.Sprintf(" %s", af))
-	}
-
-	sb.WriteString(g.prettyFullMessage())
-
-	return sb.String()
 }
 
 // versionInfo will display the compile/build-time version variables. This is
