e0dcb91853c412f05799f9366589800c3ba324db
João Pereira <484633+joaodrp@users.noreply.github.com>
2019-04-13 09:12:28 -0700

    Add simple pretty printer (#13)
    Resolves #6 and #12

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
+	"math"
 	"os"
+	"sort"
+	"strings"
 	"text/tabwriter"
+	"time"
 )
 
 var (
@@ -32,39 +37,285 @@ var (
 		6: "INFO",
 		7: "DEBUG",
 	}
+
+	// Additional fields that have a special behaviour
+	specialFields = map[string]string{
+		"app":    "_app",
+		"logger": "_logger",
+	}
 )
 
-type prettyPrinter struct {
-	reader *bufio.Scanner
-	writer io.Writer
+const timeFormat = "2006-01-02 15:04:05.000"
+
+type fullMessage string
+
+func (fm fullMessage) String() string {
+	if len(fm) == 0 {
+		return ""
+	}
+
+	var sb strings.Builder
+	sb.WriteString("\n\t")
+	sb.WriteString(strings.ReplaceAll(string(fm), "\n", "\n\t"))
+
+	return sb.String()
 }
 
-func newPrettyPrinter(r io.Reader, w io.Writer) *prettyPrinter {
-	return &prettyPrinter{
+type timestamp struct {
+	epoch    float64
+	location *time.Location
+}
+
+func (t timestamp) String() string {
+	sec, dec := math.Modf(t.epoch)
+	aux := time.Unix(int64(sec), int64(dec*(1e9))).In(t.location)
+	return fmt.Sprintf("[%s]", aux.Format(timeFormat))
+}
+
+type syslogLevel int
+
+func (l syslogLevel) String() string {
+	return levelToName[int(l)]
+}
+
+type additionalField struct {
+	key   string
+	value interface{}
+}
+
+func (af additionalField) String() string {
+	return fmt.Sprintf("%s=%v", strings.TrimPrefix(af.key, "_"), af.value)
+}
+
+func (af additionalField) special() bool {
+	for _, v := range specialFields {
+		if af.key == v {
+			return true
+		}
+	}
+	return false
+}
+
+type additionalFields []additionalField
+
+func (afs additionalFields) String() string {
+	if len(afs) == 0 {
+		return ""
+	}
+
+	// sort fields by key for predictability/reproducibility
+	sort.Slice(afs, func(i, j int) bool {
+		return afs[i].key < afs[j].key
+	})
+
+	var sb strings.Builder
+	i := 0
+	for _, af := range afs {
+		if af.special() {
+			continue
+		}
+		if i > 0 {
+			sb.WriteString(" ")
+		}
+		sb.WriteString(af.String())
+		i++
+	}
+	return sb.String()
+}
+
+type gelf struct {
+	version          string
+	host             string
+	shortMessage     string
+	fullMessage      fullMessage
+	timestamp        timestamp
+	level            syslogLevel
+	additionalFields additionalFields
+}
+
+type dict map[string]interface{}
+
+func (d dict) _findByKey(key string, required bool) (interface{}, error) {
+	val, ok := d[key]
+	if !ok && required {
+		return nil, fmt.Errorf("%s not found", key)
+	}
+	return val, nil
+}
+
+func (d dict) findByKeyAndCastToString(key string, required bool) (string, error) {
+	val, err := d._findByKey(key, required)
+	if err != nil || val == nil {
+		return "", err
+	}
+	s, ok := val.(string)
+	if !ok {
+		return "", fmt.Errorf("%s is not a valid string", key)
+	}
+	delete(d, key)
+	return s, nil
+}
+
+func (d dict) findByKeyAndCastToFloat64(key string, required bool) (float64, error) {
+	val, err := d._findByKey(key, required)
+	if err != nil || val == nil {
+		return 0, err
+	}
+	n, ok := val.(float64)
+	if !ok {
+		return 0, fmt.Errorf("%s is not a valid number", key)
+	}
+	delete(d, key)
+	return n, nil
+}
+
+func (g *gelf) UnmarshalJSON(data []byte) error {
+	d := dict{}
+	_ = json.Unmarshal(data, &d) // if it gets here it never fails
+
+	v, err := d.findByKeyAndCastToString("version", true)
+	if err != nil {
+		return err
+	}
+	g.version = v
+
+	h, err := d.findByKeyAndCastToString("host", true)
+	if err != nil {
+		return err
+	}
+	g.host = h
+
+	sm, err := d.findByKeyAndCastToString("short_message", true)
+	if err != nil {
+		return err
+	}
+	g.shortMessage = sm
+
+	fm, err := d.findByKeyAndCastToString("full_message", false)
+	if err != nil {
+		return err
+	}
+	g.fullMessage = fullMessage(fm)
+
+	t, err := d.findByKeyAndCastToFloat64("timestamp", true)
+	if err != nil {
+		return err
+	}
+	g.timestamp.epoch = t
+
+	l, err := d.findByKeyAndCastToFloat64("level", false)
+	if err != nil {
+		return err
+	}
+	if l == 0 {
+		l = 1
+	}
+	g.level = syslogLevel(l)
+
+	for k, v := range d {
+		if strings.HasPrefix(k, "_") {
+			af := additionalField{key: k, value: v}
+			g.additionalFields = append(g.additionalFields, af)
+		}
+	}
+	return nil
+}
+
+func (g *gelf) findAdditionalFieldValueByKey(key string) string {
+	for _, af := range g.additionalFields {
+		if af.key == key {
+			return af.value.(string)
+		}
+	}
+	return ""
+}
+
+func (g *gelf) app() string {
+	return g.findAdditionalFieldValueByKey(specialFields["app"])
+}
+
+func (g *gelf) logger() string {
+	return g.findAdditionalFieldValueByKey(specialFields["logger"])
+}
+
+func (g *gelf) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("%s %s: ", g.timestamp, g.level))
+
+	a := g.app()
+	if len(a) > 0 {
+		sb.WriteString(a)
+	}
+
+	l := g.logger()
+	if len(l) > 0 {
+		if len(a) > 0 {
+			sb.WriteString("/")
+		}
+		sb.WriteString(l)
+	}
+
+	if h := g.host; len(h) > 0 {
+		if len(a) > 0 || len(l) > 0 {
+			sb.WriteString(" on ")
+		}
+		sb.WriteString(h)
+	}
+
+	sb.WriteString(fmt.Sprintf(": %s", g.shortMessage))
+	sb.WriteString(fmt.Sprintf(" %s", g.additionalFields))
+	sb.WriteString(g.fullMessage.String())
+
+	return sb.String()
+}
+
+type prettyPrinter struct {
+	reader       *bufio.Scanner
+	writer       io.Writer
+	timeLocation *time.Location
+}
+
+func newPrettyPrinter(r io.Reader, w io.Writer, l *time.Location) *prettyPrinter {
+	pp := prettyPrinter{
 		reader: bufio.NewScanner(r),
 		writer: w,
 	}
+	if l == nil {
+		l = time.Local
+	}
+	pp.timeLocation = l
+	return &pp
+}
+
+func (h *prettyPrinter) processLine(b []byte) error {
+	g := &gelf{}
+	if err := json.Unmarshal(b, g); err != nil {
+		return err
+	}
+	g.timestamp.location = h.timeLocation
+	if _, err := fmt.Fprintln(h.writer, g); err != nil {
+		return err
+	}
+	return nil
 }
 
 func (h *prettyPrinter) run() error {
 	for h.reader.Scan() {
-		t := h.reader.Text()
-		if len(t) == 0 {
+		b := h.reader.Bytes()
+		if len(b) == 0 {
 			continue
 		}
-		if err := h.processLine(t); err != nil {
-			return err
+		if err := h.processLine(b); err != nil {
+			if _, err := fmt.Fprintln(h.writer, string(b)); err != nil {
+				return err
+			}
+			continue
 		}
 	}
 	if err := h.reader.Err(); err != nil {
 		return err
 	}
 	return nil
-}
-
-func (h *prettyPrinter) processLine(l string) error {
-	_, err := fmt.Fprintln(h.writer, l)
-	return err
 }
 
 // versionInfo will display the compile/build-time version variables. This is
@@ -96,7 +347,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	pp := newPrettyPrinter(os.Stdin, os.Stdout)
+	pp := newPrettyPrinter(os.Stdin, os.Stdout, nil)
 	if err := pp.run(); err != nil {
 		panic(err)
 	}
--- main_test.go
+++ main_test.go
@@ -2,28 +2,20 @@ package main
 
 import (
 	"bytes"
+	"flag"
+	"github.com/andreyvit/diff"
 	"io"
-	"strings"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
 )
 
-func TestLevelToName(t *testing.T) {
-	wanted := map[int]string{
-		0: "EMERGENCY",
-		1: "ALERT",
-		2: "CRITICAL",
-		3: "ERROR",
-		4: "WARNING",
-		5: "NOTICE",
-		6: "INFO",
-		7: "DEBUG",
-	}
-	for k, v := range levelToName {
-		if v != wanted[k] {
-			t.Errorf("invalid level name, wanted %v got %s", wanted[k], v)
-		}
-	}
-}
+var (
+	create = flag.Bool("create", false, "create .golden files")
+	update = flag.Bool("update", false, "update .golden files")
+)
 
 func TestVersionInfo(t *testing.T) {
 	Version = "0.0.0"
@@ -41,45 +33,6 @@ func TestVersionInfo(t *testing.T) {
 	}
 }
 
-func TestPrettyPrinter_run(t *testing.T) {
-	tests := []struct {
-		name string
-		in   []string
-		out  []string
-	}{
-		{"simple", []string{"foo", "bar"}, []string{"foo", "bar"}},
-		{"skipBlanks", []string{"foo", "", "bar"}, []string{"foo", "bar"}},
-	}
-
-	stdin := new(bytes.Buffer)
-	stdout := new(bytes.Buffer)
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			for _, i := range tt.in {
-				stdin.WriteString(i)
-				stdin.WriteString("\n")
-			}
-
-			pp := newPrettyPrinter(stdin, stdout)
-			if err := pp.run(); err != nil {
-				t.Fatalf("unwanted error: %v", err)
-			}
-
-			out := strings.Split(stdout.String(), "\n")
-			out = out[:len(out)-1]
-			for i, o := range out {
-				if o != tt.out[i] {
-					t.Errorf("wanted %s got %s", out[i], o)
-				}
-			}
-
-			stdin.Reset()
-			stdout.Reset()
-		})
-	}
-}
-
 type readerErrMock struct{}
 
 func (readerErrMock) Read(p []byte) (int, error) {
@@ -87,7 +40,7 @@ func (readerErrMock) Read(p []byte) (int, error) {
 }
 
 func TestPrettyPrinter_run_readError(t *testing.T) {
-	pp := newPrettyPrinter(readerErrMock{}, &bytes.Buffer{})
+	pp := newPrettyPrinter(readerErrMock{}, &bytes.Buffer{}, nil)
 	err := pp.run()
 
 	if err == nil {
@@ -108,7 +61,7 @@ func TestPrettyPrinter_run_writeError(t *testing.T) {
 	stdin := new(bytes.Buffer)
 	stdin.WriteString("foo\n")
 
-	pp := newPrettyPrinter(stdin, &writerErrMock{})
+	pp := newPrettyPrinter(stdin, &writerErrMock{}, nil)
 	err := pp.run()
 
 	if err == nil {
@@ -116,5 +69,108 @@ func TestPrettyPrinter_run_writeError(t *testing.T) {
 	}
 	if err != io.ErrShortWrite {
 		t.Errorf("wanted %q got %v", io.ErrShortWrite, err)
+	}
+}
+
+func TestPrettyPrinter_processLine_writeError(t *testing.T) {
+	pp := newPrettyPrinter(new(bytes.Buffer), &writerErrMock{}, nil)
+	input := "{\"version\":\"1.1\",\"host\":\"example.org\"," +
+		"\"short_message\":\"foo\",\"timestamp\":1385053862.3072,\"level\":6}\n"
+	err := pp.processLine([]byte(input))
+
+	if err == nil {
+		t.Fatal("write should have failed")
+	}
+	if err != io.ErrShortWrite {
+		t.Errorf("wanted %q got %v", io.ErrShortWrite, err)
+	}
+}
+
+func _loadFixture(name string) ([]byte, error) {
+	path := filepath.Join("testdata", name)
+	return ioutil.ReadFile(path)
+}
+
+func loadInputFixture(t *testing.T, name string) []byte {
+	fx, err := _loadFixture(name)
+	if err != nil {
+		t.Fatalf("error reading input fixture %s: %v", name, err)
+	}
+	return fx
+}
+
+func createGoldenFile(t *testing.T, name string) {
+	path := filepath.Join("testdata", name)
+	f, err := os.Create(path)
+	if err != nil {
+		t.Fatalf("error creating golden file %s: %v", name, err)
+	}
+	_ = f.Close()
+}
+
+func updateGoldenFile(t *testing.T, name string, actual []byte) {
+	path := filepath.Join("testdata", name)
+	if err := ioutil.WriteFile(path, actual, 0644); err != nil {
+		t.Fatalf("error updating golden file %s: %v", name, err)
+	}
+}
+
+func loadGoldenFile(t *testing.T, name string, actual []byte) []byte {
+	if *update {
+		updateGoldenFile(t, name, actual)
+	}
+	g, err := _loadFixture(name)
+	if err != nil {
+		if _, ok := err.(*os.PathError); !ok || !*create {
+			t.Fatalf("error reading golden file %s: %v", name, err)
+		}
+		createGoldenFile(t, name)
+		loadGoldenFile(t, name, actual)
+	}
+	return g
+}
+
+func TestPrettyPrinter_run(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		out  string
+	}{
+		{
+			name: "valid",
+			in:   "valid.input",
+			out:  "valid.golden",
+		},
+		{
+			name: "invalid",
+			in:   "invalid.input",
+			out:  "invalid.golden",
+		},
+	}
+
+	stdin := new(bytes.Buffer)
+	stdout := new(bytes.Buffer)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			in := loadInputFixture(t, tt.in)
+			stdin.Write(in)
+
+			pp := newPrettyPrinter(stdin, stdout, time.UTC)
+			if err := pp.run(); err != nil {
+				t.Fatalf("unwanted error: %v", err)
+			}
+
+			actual := stdout.Bytes()
+			expected := loadGoldenFile(t, tt.out, actual)
+
+			if res := bytes.Compare(actual, expected); res != 0 {
+				d := diff.LineDiff(string(expected), string(actual))
+				t.Errorf("output not as expected:\n%v", d)
+			}
+
+			stdin.Reset()
+			stdout.Reset()
+		})
 	}
 }
